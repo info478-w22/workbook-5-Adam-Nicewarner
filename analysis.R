d4f55e2d6c3bb0900338ddf3a211d51a9adf60ea
Adam Nicewarner <nicewa@uw.edu>
2022-03-04 15:44:08 -0800

    second half done

--- analysis.R
+++ analysis.R
@@ -4,12 +4,27 @@
 library(foreign)
 library(survey)
 library(Hmisc)
+library(openintro)
+library(tidyverse)
+library(ggplot2)
 
 demog <- sasxport.get('DEMO_I.XPT')
 alc <- sasxport.get('ALQ_I.XPT')
-merged <- merge(x = demog, y = alc, by = "seqn")
+merged <- merge(x = demog, y = alc, by = "seqn", all=TRUE)
 
 # The sample weight for demographics is wtint2yr or wtmec2yr, showing how we should weigh these numbers to try and get a representative sample of the population. 
 sum(demog$wtint2yr)
 sum(demog$wtmec2yr)
 # These numbers are different, but sum up to be the same, they should be about the population of america, around 300 million. 
+# They represent slightly different things in the survey, but you would want to use the MEC one for this analysis, as it references things asked about in the MEC survey.
+
+merged$alq151(merged$alq151 ==2) <- 0
+merged$alq151(merged$alq151 ==7) <- NA
+merged$alq151(merged$alq151 ==9) <- NA
+
+design <- svydesign(id=~sdmvpsu, nest=TRUE, strata=~sdmvstra, weights=~wtint2yr,data=merged)
+
+  
+nhanes_mean <- svymean(~alq151, design, na.rm=TRUE)
+
+gender_perc <- svyby(~alq151, ~riagendr ,design, svymean, na.rm=TRUE)
